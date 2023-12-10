35fd51d7bcd9ecf8d560d16d1d5135a90fd58632
Urjashee Shaw <urjashee09@gmail.com>
2023-12-10 14:01:20 +0530

    index as landing page

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!!')
+})
